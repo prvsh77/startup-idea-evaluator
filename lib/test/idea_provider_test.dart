fb757cce3615eed71cd7d48611661a03f41a6551
M Prashant Rao <mprashantrao9@gmail.com>
2026-01-04 18:39:26 +0530

    Update idea_provider_test.dart

--- lib/test/idea_provider_test.dart
+++ lib/test/idea_provider_test.dart
@@ -1,1 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:startup_idea_evaluator/providers/idea_provider.dart';
 
+void main() {
+  group('Idea Provider Tests', () {
+    test('Initial state is empty', () {
+      final provider = IdeaProvider();
+      expect(provider.ideas, isEmpty);
+    });
+  });
+}
