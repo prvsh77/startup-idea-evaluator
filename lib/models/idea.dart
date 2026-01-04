502da5f01b74e461b067b7d0086f32b6154356cd
M Prashant Rao <mprashantrao9@gmail.com>
2026-01-04 18:35:17 +0530

    Update idea.dart

--- lib/models/idea.dart
+++ lib/models/idea.dart
@@ -1,1 +1,43 @@
+class Idea {
+  final String id;
+  final String name;
+  final String tagline;
+  final String description;
+  final int rating;
+  final int votes;
+  final DateTime createdAt;
+  final String? category;
 
+  Idea({
+    required this.id,
+    required this.name,
+    required this.tagline,
+    required this.description,
+    required this.rating,
+    required this.votes,
+    required this.createdAt,
+    this.category,
+  });
+
+  Idea copyWith({
+    String? id,
+    String? name,
+    String? tagline,
+    String? description,
+    int? rating,
+    int? votes,
+    DateTime? createdAt,
+    String? category,
+  }) {
+    return Idea(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      tagline: tagline ?? this.tagline,
+      description: description ?? this.description,
+      rating: rating ?? this.rating,
+      votes: votes ?? this.votes,
+      createdAt: createdAt ?? this.createdAt,
+      category: category ?? this.category,
+    );
+  }
+}
